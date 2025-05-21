27fec7d0bcde9d06f5fbf6b853f73c69cb759b80
exif <5650537+Exifs@users.noreply.github.com.>
2025-05-21 16:34:51 +0200

    added exclude and include patterns to tree tool

--- server.py
+++ server.py
@@ -31,21 +31,26 @@ async def gitingest_repo(
 
 @mcp.tool()
 async def gitingest_tree(
-        source: str = "http://github.com/cyclotruc/gitingest"
+        source: str = "http://github.com/cyclotruc/gitingest",
+        include_patterns: str | None = None,
+        exclude_patterns: str | None = None
 ) -> str:
     """
     Show only the tree structure of a git repository.
 
     Args:
         repo_path_or_url: A GitHub URL or local path
+        include_patterns: Optional patterns to include (comma separated) e.g.: ".java,.ts,.js,.xml"
+        exclude_patterns: Optional patterns to exclude (comma separated) e.g.: ".jpeg,.gif,.jpg,.png,.svg,.mp4,.bin"
 
     Returns:
         str: Summary and tree structure of the repository
     """
     assert source
-    _, tree, _ = await ingest_async(source=source)
+    _, tree, _ = await ingest_async(source=source,
+                                    include_patterns=include_patterns,
+                                    exclude_patterns=exclude_patterns)
     return f"{tree}"
-
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
