9b3f15ffdbbb55e4c24e159791b1882171035bab
exif <5650537+Exifs@users.noreply.github.com.>
2025-05-21 16:56:03 +0200

    removed summary for tree and repo tools added tool for summary only

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ async def gitingest_repo(
         exclude_patterns: Optional patterns to exclude (comma separated) e.g.: ".jpeg,.gif,.jpg,.png,.svg,.mp4,.bin"
 
     Returns:
-        str: Summary and Content digest of the repository
+        str: Content digest of the repository
     """
     assert source
     summary, _, content = await ingest_async(
@@ -26,7 +26,7 @@ async def gitingest_repo(
         include_patterns=include_patterns,
         exclude_patterns=exclude_patterns
     )
-    return f"{summary}\n\n{content}"
+    return content
 
 
 @mcp.tool()
@@ -44,13 +44,36 @@ async def gitingest_tree(
         exclude_patterns: Optional patterns to exclude (comma separated) e.g.: ".jpeg,.gif,.jpg,.png,.svg,.mp4,.bin"
 
     Returns:
-        str: Summary and Tree structure of the repository
+        str: Tree structure of the repository
     """
     assert source
     summary, tree, _ = await ingest_async(source=source,
                                     include_patterns=include_patterns,
                                     exclude_patterns=exclude_patterns)
-    return f"{summary}\n\n{tree}"
+    return tree
+
+@mcp.tool()
+async def gitingest_summary(
+        source: str = "http://github.com/cyclotruc/gitingest",
+        include_patterns: str | None = None,
+        exclude_patterns: str | None = None
+) -> str:
+    """
+    Show a summary of the repository content (repository name, number of files analyzed and estimated token number)
+
+    Args:
+        repo_path_or_url: A GitHub URL or local path
+        include_patterns: Optional patterns to include (comma separated) e.g.: ".java,.ts,.js,.xml"
+        exclude_patterns: Optional patterns to exclude (comma separated) e.g.: ".jpeg,.gif,.jpg,.png,.svg,.mp4,.bin"
+
+    Returns:
+        str: Summary and Tree structure of the repository
+    """
+    assert source
+    summary, _, _ = await ingest_async(source=source,
+                                    include_patterns=include_patterns,
+                                    exclude_patterns=exclude_patterns)
+    return summary
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
